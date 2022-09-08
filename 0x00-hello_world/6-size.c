ab73441be0f1d1c596870266383bb8e2948a0895
Mechanics01 <suleemmanuel10@gmail.com>
2022-09-08 16:50:08 -0700

    -

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,3 +6,18 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
+	printf("Size of char: %d bytes(s)\n", sizeof(d));
+	printf("Size of int: %d bytes(s)\n", sizeof(a));
+	printf("Size of long int: %d bytes(s)\n", sizeof(b);
+	printf("Size of long long int: %d bytes(s)\n", sizeof(c));
+	printf("Size of float: %d bytes(s)\n", sizeof(f));
+
+	return (0);
+}
