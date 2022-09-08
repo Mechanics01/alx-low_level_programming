5f09ff870c629333f87f78d1ceb6584a3942f8a1
Mechanics01 <suleemmanuel10@gmail.com>
2022-09-08 16:54:07 -0700

    -

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 
 	printf("Size of char: %d bytes(s)\n", sizeof(d));
 	printf("Size of int: %d bytes(s)\n", sizeof(a));
-	printf("Size of long int: %d bytes(s)\n", sizeof(b);
+	printf("Size of long int: %d bytes(s)\n", sizeof(b));
 	printf("Size of long long int: %d bytes(s)\n", sizeof(c));
 	printf("Size of float: %d bytes(s)\n", sizeof(f));
 
