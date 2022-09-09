b8505a1808feb58c97d87694d61a6aced1f88719
Mechanics01 <suleemmanuel10@gmail.com>
2022-09-09 15:02:21 -0700

    -

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, k;
+
+	for (i = '0'; i < '9'; i++)
+	{
+		for (k = i + 1; k <= '9'; k++)
+		{
+			if (i != k)
+			{
+				putchar(i);
+				putchar(k);
+			
+				if (i == 8 && k == 9)
+					continue;
+			
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,17 +9,13 @@
  */
 int main(void)
 {
+	char low;
 	int i;
 
 	for (i = '0'; i <= '9'; i++)
-	{
 		putchar(i);
-		if (i != '9')
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
+	for (low = 'a'; low <= 'f'; low++)
+		putchar(low);
 	putchar('\n');
 	return (0);
 }
