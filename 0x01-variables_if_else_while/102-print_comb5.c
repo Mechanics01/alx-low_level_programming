3f80c34b3ef1150b67b344ec0d7f43639040baf9
Mechanics01 <suleemmanuel10@gmail.com>
2022-09-09 15:35:57 -0700

    -

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,16 +11,16 @@ int main(void)
 {
 	int i, k;
 
-	for (i = '0'; i < '98'; i++)
+	for (i = 0; i < 98; i++)
 	{
-		for (k = i + 1; k <= '99'; k++)
+		for (k = i + 1; k <= 99; k++)
 		{
 			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
 			putchar(' ');
 			putchar((k / 10) + '0');
 			putchar((k % 10) + '0');
-			if (i == '88' && k == '99')
+			if (i == 88 && k == t 99)
 				continue;
 			putchar(',');
 			putchar(' ');
