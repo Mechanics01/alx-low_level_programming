bc1dc2fd534233fcf7b5383f971e03e2a16252de
Mechanics01 <suleemmanuel10@gmail.com>
2022-09-09 15:32:58 -0700

    -

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, k;
+
+	for (i = '0'; i < '98'; i++)
+	{
+		for (k = i + 1; k <= '99'; k++)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((k / 10) + '0');
+			putchar((k % 10) + '0');
+			if (i == '88' && k == '99')
+				continue;
+			putchar(',');
+			putchar(' ');
+				
+		}
+	}
+	putchar('\n');
+	return (0);
+}
