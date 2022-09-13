b721349861ba4c9895b1349c728b2c608c8ad3bb
Mechanics01 <suleemmanuel10@gmail.com>
2022-09-13 07:47:42 -0700

    -

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,12 +4,11 @@
  *
  * Return: Always 0 (Success)
  */
-void  print_alphabet(void)
+void print_alphabet(void)
 {
 	char a;
 
 	for (a = 'a'; a <= 'z'; a++)
 		_putchar(a);
 	_putchar('\n');
-	return (0);
 }
