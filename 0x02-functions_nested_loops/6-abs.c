b6d84890053e264e406f8264f31adfd8b6d78ed0
Mechanics01 <suleemmanuel10@gmail.com>
2022-09-13 11:54:12 -0700

    -

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - prints the absolute value of the function
+ * @n: letter whose absoulte value is needed
+ * Return: Absoulte values of given input 
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+	return (0);
+}
