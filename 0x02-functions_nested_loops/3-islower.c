29e6a657b491374dbb30a83d7173549019e0ad94
Mechanics01 <suleemmanuel10@gmail.com>
2022-09-13 11:00:38 -0700

    -

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,12 @@
 /**
  * _islower - checks for a lowercase character
  *
- * Return: Always 0 
+ * Return: Always 0
  */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 		return (1);
 	else
-		return(0);
+		return (0);
 }
