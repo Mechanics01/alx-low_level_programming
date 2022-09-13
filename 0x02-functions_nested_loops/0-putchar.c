ddee84916c8516f489ed3bf174cebc99395dc9ac
Mechanics01 <suleemmanuel10@gmail.com>
2022-09-13 07:19:33 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <main.h>>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char s = ["_putchar"];
+	int a = 0;
+
+	for (a = 0; a <= 7; a++)
+		_putchar(s[a]);
+	return 0;
+}
