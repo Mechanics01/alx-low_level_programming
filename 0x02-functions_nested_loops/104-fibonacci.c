ce3a8e335b9964bf0c51a495545f34314c4412e6
Mechanics01 <suleemmanuel10@gmail.com>
2022-09-14 07:30:58 -0700

    -

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -29,8 +29,9 @@ int main(void)
 			hold2 = bk1 + bk2;
 			bk1 = bk2, bk2 = hold2;
 			printf("%lu", bk2);
-		}											if (count != 97)
-			printf(", ");
+		}
+		if (count != 97)
+		printf(", ");
 	}
 	printf("\n");
 	return (0);
