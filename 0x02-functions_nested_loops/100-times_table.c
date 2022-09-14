accbc8328281bf4b4b825bd7a5770861c2bc7ed4
Mechanics01 <suleemmanuel10@gmail.com>
2022-09-13 21:15:30 -0700

    -

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@ void print_times_table(int n)
 	int i;
 	int j;
 
-	if (n > 0 && n < 15)
+	if (n >= 0 && n < 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -17,8 +17,7 @@ void print_times_table(int n)
 			for (j = 1; j <= n; j++)
 				putformat(i * j);
 			_putchar('\n');
-		}
-		
+				
 	}
 }
 /**
