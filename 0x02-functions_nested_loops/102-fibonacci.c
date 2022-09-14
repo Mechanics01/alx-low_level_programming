55027be8ea8fad0b885e84dfc49f3345ce0abc8f
Mechanics01 <suleemmanuel10@gmail.com>
2022-09-13 21:49:39 -0700

    -

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,7 +10,6 @@ int main(void)
 {
 	int i;
 	long int fibonacci[50];
-	
 	fibonacci[0] = 1;
 	fibonacci[1] = 2;
 	printf("%ld, %ld, ", fibonacci[0], fibonacci[1]);
@@ -18,7 +17,7 @@ int main(void)
 	for (i = 2; i < 50; i++)
 	{
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-		if (i == 49)					
+		if (i == 49)
 			printf("%ld\n", fibonacci[i]);
 		else
 			printf("%ld, ", fibonacci[i]);
