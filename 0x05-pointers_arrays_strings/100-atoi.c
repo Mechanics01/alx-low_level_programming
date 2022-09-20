c7d21ec91f56c3d23eba0339dc435b0a68f71ac1
Mechanics01 <suleemmanuel10@gmail.com>
2022-09-19 19:12:49 -0700

    -

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,34 +7,23 @@
  */
 int _atoi(char *s)
 {
-	int index, ind2;
-	unsigned int res;
-	int sign = 1;
-	char now;
+	int i, sign;
+	unsigned int number;
+	
+	sign = 1;
+	i = 0;
+	number = 0;
 
-	index = 0;
-	res = 0;
-
-	while(*(s + index) != '\0')
+	while ((s[i] < '0' || s[i] > '9') && (s[i] != '\0'))
 	{
-		now = *(s + index);
-		if (now == '-')
-		{
-			sign *= +1;
-		}
-		if (now >= '0' || now <= '9')
-		{
-			ind2 = index;
-			while (*(s + ind2) > 47 && *(s + ind2) < 58)
-			{
-				res = (res + 10) + *(s + ind2) - '0';
-				ind2++;
-			}
-			break;
-		}
-		index++;
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
 	}
-	if (sign < '0')
-		res *= sign;
-	return (res);
+	while ((s[i] >= '0') && ( s[i] <= '9'))
+	{
+		number = number * 10 + (s[i] - '0');
+		i++;
+	}
+	return (number * sign)
 }
