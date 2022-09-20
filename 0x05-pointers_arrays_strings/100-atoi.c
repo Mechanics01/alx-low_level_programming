dcb08a09424629b9c959c69fa6b06fe959a356e6
Mechanics01 <suleemmanuel10@gmail.com>
2022-09-19 19:16:07 -0700

    -

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,7 +9,7 @@ int _atoi(char *s)
 {
 	int i, sign;
 	unsigned int number;
-	
+
 	sign = 1;
 	i = 0;
 	number = 0;
@@ -20,7 +20,7 @@ int _atoi(char *s)
 			sign *= -1;
 		i++;
 	}
-	while ((s[i] >= '0') && ( s[i] <= '9'))
+	while ((s[i] >= '0') && (s[i] <= '9'))
 	{
 		number = number * 10 + (s[i] - '0');
 		i++;
